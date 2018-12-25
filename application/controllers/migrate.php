5e299222b67d2b8c8d1480d2388636f971eaa4ca
RifqiMus <46133429+RifqiMus@users.noreply.github.com>
2018-12-25 23:01:44 +0700

    Update migrate.php

--- application/controllers/migrate.php
+++ application/controllers/migrate.php
@@ -1,12 +1,22 @@
-<?php defined("BASEPATH") or exit("No direct script access allowed");
+<?php  if ( ! defined('BASEPATH')) exit("No direct script access allowed");
 
-class Migrate extends CI_Controller{
+class Migrate extends CI_Controller {
 
-    public function index($version){
-        $this->load->library("migration");
+  public function __construct()
+  {
+    parent::__construct();
 
-      if(!$this->migration->version($version)){
-          show_error($this->migration->error_string());
-      }   
+    $this->input->is_cli_request() 
+      or exit("Execute via command line: php index.php migrate");
+
+    $this->load->library('migration');
+  }
+
+  public function index()
+  {
+    if(!$this->migration->latest()) 
+    {
+      show_error($this->migration->error_string());
     }
+  }
 }
